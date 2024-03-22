713a04e72d0e5dc859390ea3003aec2552779f70
oleg_solovev_2 <leg2012@bk.ru>
2024-03-22 22:34:22 +0300

    #02 add modal window validation

--- js/main.js
+++ js/main.js
@@ -45,3 +45,49 @@ const swiper = new Swiper("#gallery", {
     }
 })
 
+// Валидация модального окна
+let validation = new JustValidate('#form')
+
+let selector = document.querySelector("#phone")
+let im = new Inputmask("+7(999)999-99-99")
+im.mask(selector)
+
+validation.addField('#name', [
+    {
+        rule: "required",
+        errorMessage: 'Введите имя'
+    },
+    {
+        rule: "minLength",
+        value: 2,
+        errorMessage: 'Минимум 2 символа'
+    }
+])
+.addField('#phone', [
+    {
+       validator: (value) => {
+        const phone = selector.inputmask.unmaskedvalue()
+        return Boolean(Number(phone) && phone.length > 0)
+       },
+       errorMessage: 'Введите телефон'
+    },
+    {
+        validator: (value) => {
+         const phone = selector.inputmask.unmaskedvalue()
+         return Boolean(Number(phone) && phone.length === 10)
+        },
+        errorMessage: 'Недопустимый формат'
+     }
+])
+// .addField('#email', [
+//     {
+//         rule: "required",
+//         errorMessage: 'Введите почту'
+//     },
+//     {
+//         rule: "email",
+//         errorMessage: 'Ошибка в адресе почты'
+//     }
+// ])
+
+
